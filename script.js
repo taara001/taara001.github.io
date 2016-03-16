2f3b25a06e8c8452899a1458a0c181a5df56a811
taara001 <tarjei.strand.arntsen@gmail.com>
2016-03-16 19:41:37 +0100

    (2.9)

--- script.js
+++ script.js
@@ -2,8 +2,22 @@
     'use strict';
     
     var el = function (id) { 
-    document.getElementById(id + "")
-    }
+        document.getElementById(id + "")
+    };
+    
+    // modell
+    var spiller = "X"
+    var trekk = new Array(9);
+    
+    const brett = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+    
+    
+    var oppdater = function(knappTrykket) { 
+        console.log("Knapp trykket med id: "knappTrykket);
+        settSpiller();
+        Lagretrekket
+        //vis
+         };
     
     el("0").addEventListener("click", function () {
         console.log("Hello Thing 0")
